f027d80626cdce27e57640f03983b33d7b46f435
Uros Perisic <uros.m.perisic@gmail.com>
2022-05-08 21:52:16 +0800

    Implement mint (broken)

--- contracts/MembershipERC721.sol
+++ contracts/MembershipERC721.sol
@@ -8,47 +8,72 @@ import "./fractional/OpenZeppelin/token/ERC20/IERC20.sol";
 contract MembershipERC721 is ERC721, Ownable {
     IERC20 public erc20;
 
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        address erc20_,
-        string baseURI_
-    ) ERC721(name_, symbol_) {
-        _setBaseURI(baseURI_);
-        erc20 = IERC20(erc20_);
-    }
-
-    enum Tier {
+    enum TierType {
         GENESIS,
         FOUNDATION,
         FRIEND
     }
 
-    struct TierTracker {
+    struct Tier {
         uint16 currId;
         uint16 maxId;
         uint16 price;
         uint16[] refundedIds;
     }
 
-    // TODO: convert to mapping
-    TierTracker[3] private tiers = [
-        TierTracker({currId: 0, maxId: 99, price: 40000, refundedIds: []}),
-        TierTracker({currId: 100, maxId: 1099, price: 4000, refundedIds: []}),
-        TierTracker({currId: 1100, maxId: 11099, price: 400, refundedIds: []})
-    ];
+    mapping(TierType => Tier) private tiers;
 
-    function _nextID(Tier tier_) private returns (uint16 memory) {
-        TierTracker memory tier = tiers[tier_];
-        uint16 memory res;
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address erc20_,
+        string memory baseURI_
+    ) ERC721(name_, symbol_) {
+        _setBaseURI(baseURI_);
+        erc20 = IERC20(erc20_);
 
-        if (tiers[tier].currId < tiers[tier].maxId) {
+        tiers[TierType.GENESIS] = Tier({
+            currId: 0,
+            maxId: 99,
+            price: 40000,
+            refundedIds: []
+        });
+        tiers[TierType.FOUNDATION] = Tier({
+            currId: 100,
+            maxId: 1099,
+            price: 4000,
+            refundedIds: []
+        });
+        tiers[TierType.FRIEND] = Tier({
+            currId: 1100,
+            maxId: 11099,
+            price: 400,
+            refundedIds: []
+        });
+    }
+
+    function _nextID(TierType tier_) private returns (uint16) {
+        Tier memory tier = tiers[tier_];
+        uint16 res;
+
+        if (tier.currId < tier.maxId) {
             res = tier.currId;
             tier.currId++;
         } else {
+            // TODO: check if it is reverted properly
+            // TODO: does empty array cause revert as expected?
             res = tier.refundedIds.pop();
         }
 
         return res;
+    }
+
+    function mint(TierType tier_) public {
+        Tier memory tier = tiers[tier_];
+        uint256 price = tier.price * erc20.decimals();
+        require(erc20.balanceOf(msg.sender) >= price, "insufficient balance");
+        erc20.approve(address(this), price);
+        erc20.transferFrom(msg.sender, address(this), price);
+        _safeMint(msg.sender, _nextID());
     }
 }
