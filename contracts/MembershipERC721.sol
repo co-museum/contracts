ff9ac8a4b479ac40cd845d1b86ce4d2f6608d2b7
Alaukik Pant <alaukik.pant@ninjavan.co>
2022-05-11 17:50:52 +0800

    lock ERC20s in the contract for an NFT

--- contracts/MembershipERC721.sol
+++ contracts/MembershipERC721.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "./fractional/OpenZeppelin/access/Ownable.sol";
 import "./fractional/OpenZeppelin/token/ERC721/ERC721.sol";
 import "./fractional/OpenZeppelin/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract MembershipERC721 is ERC721, Ownable {
     IERC20 public erc20;
@@ -15,11 +16,16 @@ contract MembershipERC721 is ERC721, Ownable {
     }
 
     struct Tier {
+        TierType tierType;
         uint16 currId;
         uint16 maxId;
         uint16 price;
         uint16[] refundedIds;
     }
+
+    uint16[] refundedIdGenesis;
+    uint16[] refundedIdFoundation;
+    uint16[] refundedIdFriends;
 
     mapping(TierType => Tier) private tiers;
 
@@ -33,22 +39,25 @@ contract MembershipERC721 is ERC721, Ownable {
         erc20 = IERC20(erc20_);
 
         tiers[TierType.GENESIS] = Tier({
+            tierType: TierType.GENESIS,
             currId: 0,
             maxId: 99,
             price: 40000,
-            refundedIds: []
+            refundedIds: refundedIdGenesis
         });
         tiers[TierType.FOUNDATION] = Tier({
+            tierType: TierType.FOUNDATION,
             currId: 100,
             maxId: 1099,
             price: 4000,
-            refundedIds: []
+            refundedIds: refundedIdFoundation
         });
         tiers[TierType.FRIEND] = Tier({
+            tierType: TierType.FRIEND,
             currId: 1100,
             maxId: 11099,
             price: 400,
-            refundedIds: []
+            refundedIds: refundedIdFriends
         });
     }
 
@@ -56,24 +65,33 @@ contract MembershipERC721 is ERC721, Ownable {
         Tier memory tier = tiers[tier_];
         uint16 res;
 
+        refundedIdFriends.push(1);
+
         if (tier.currId < tier.maxId) {
             res = tier.currId;
             tier.currId++;
         } else {
             // TODO: check if it is reverted properly
             // TODO: does empty array cause revert as expected?
-            res = tier.refundedIds.pop();
+            // res = tier.refundedIds.pop();
         }
 
         return res;
     }
 
-    function mint(TierType tier_) public {
+    function redeem(TierType tier_) public {
+        console.log("HEREEE");
+        console.log("Message Sender", msg.sender);
+        console.log(
+            "ERC20 balance of message sender",
+            erc20.balanceOf(msg.sender)
+        );
         Tier memory tier = tiers[tier_];
-        uint256 price = tier.price * erc20.decimals();
+        uint256 price = tier.price;
+        console.log("Price of tier", price);
+
         require(erc20.balanceOf(msg.sender) >= price, "insufficient balance");
-        erc20.approve(address(this), price);
         erc20.transferFrom(msg.sender, address(this), price);
-        _safeMint(msg.sender, _nextID());
+        _safeMint(msg.sender, _nextID(tier.tierType));
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,7 @@
 // Runtime Environment's members available in the global scope.
 import { BigNumberish, utils } from "ethers";
 import { ethers } from "hardhat";
-import { TokenVault } from "../typechain";
+import { ERC20, TokenVault } from "../typechain";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -16,58 +16,106 @@ async function main() {
   // await hre.run('compile');
 
   // the first address is the default msg.sender
-  const [signer] = await ethers.getSigners()
-  console.log("Default signer:", signer.address)
+  // const [signer] = await ethers.getSigners();
+  // console.log("Default signer:", signer.address);
 
-  // We get the contract to deploy
-  const Settings = await ethers.getContractFactory("Settings");
-  const settings = await Settings.deploy();
-  await settings.deployed();
-  console.log("Settings deployed to:", settings.address);
+  // // We get the contract to deploy
+  // const Settings = await ethers.getContractFactory("Settings");
+  // const settings = await Settings.deploy();
+  // await settings.deployed();
+  // console.log("Settings deployed to:", settings.address);
 
-  const VaultFactory = await ethers.getContractFactory("ERC721VaultFactory");
-  const vaultFactory = await VaultFactory.deploy(settings.address);
-  await vaultFactory.deployed();
-  console.log("VaultFactory deployed to:", vaultFactory.address);
+  // const VaultFactory = await ethers.getContractFactory("ERC721VaultFactory");
+  // const vaultFactory = await VaultFactory.deploy(settings.address);
+  // await vaultFactory.deployed();
+  // console.log("VaultFactory deployed to:", vaultFactory.address);
 
-  const DummyNFT = await ethers.getContractFactory("ERC721PresetMinterPauserAutoId");
-  const dummyNFT = await DummyNFT.deploy("Dummy", "DMY", "co-museum.com/dummy-nft");
-  await dummyNFT.deployed();
-  console.log("DummyNFT deployed to:", dummyNFT.address);
+  // const DummyNFT = await ethers.getContractFactory(
+  //   "ERC721PresetMinterPauserAutoId"
+  // );
+  // const dummyNFT = await DummyNFT.deploy(
+  //   "Dummy",
+  //   "DMY",
+  //   "co-museum.com/dummy-nft"
+  // );
+  // await dummyNFT.deployed();
+  // console.log("DummyNFT deployed to:", dummyNFT.address);
 
-  dummyNFT.on("Transfer", async (from: string, to: string, _tokenId: BigNumberish, event: Event) => {
-    console.log("Token ID:", _tokenId)
-    const tokenId = _tokenId
-    await dummyNFT.approve(vaultFactory.address, tokenId)
+  // dummyNFT.on(
+  //   "Transfer",
+  //   async (from: string, to: string, _tokenId: BigNumberish, event: Event) => {
+  //     console.log("Token ID:", _tokenId);
+  //     const tokenId = _tokenId;
+  //     await dummyNFT.approve(vaultFactory.address, tokenId);
 
-    await vaultFactory.mint(
-      "Dummy Frac", // name
-      "DMYF", // symbol
-      dummyNFT.address, // token
-      tokenId,
-      utils.parseEther("10000"), // supply
-      10, // listPrice
-      0, // fee
-    )
+  //     await vaultFactory.mint(
+  //       "Dummy Frac", // name
+  //       "DMYF", // symbol
+  //       dummyNFT.address, // token
+  //       tokenId,
+  //       utils.parseEther("10000"), // supply
+  //       10, // listPrice
+  //       0 // fee
+  //     );
 
-    dummyNFT.removeAllListeners()
-  })
+  //     dummyNFT.removeAllListeners();
+  //   }
+  // );
 
-  vaultFactory.on("Mint",
-    async (
-      token: string,
-      id: BigNumberish,
-      listPrice: BigNumberish,
-      vault: string,
-      vaultId: BigNumberish,
-      event: Event
-    ) => {
-      console.log("Fractionalised token address:", vault)
-      vaultFactory.removeAllListeners()
-    }
-  )
+  // var tokenAddress = "";
 
-  await dummyNFT.mint(signer.address)
+  // await vaultFactory.on(
+  //   "Mint",
+  //   async (
+  //     token: string,
+  //     id: BigNumberish,
+  //     listPrice: BigNumberish,
+  //     vault: string,
+  //     vaultId: BigNumberish,
+  //     event: Event
+  //   ) => {
+  //     console.log("Fractionalised token address:", vault);
+  //     tokenAddress = vault;
+  //     vaultFactory.removeAllListeners();
+  //   }
+  // );
+
+  // await dummyNFT.mint(signer.address);
+
+  const [signer1, signer2] = await ethers.getSigners();
+  console.log("Signer1 address:", signer1.address);
+  console.log("Signer2 address:", signer2.address);
+
+  const SampleERC20 = await ethers.getContractFactory("GLDToken");
+  const sampleERC20 = await SampleERC20.deploy(4000000);
+  await sampleERC20.deployed();
+  console.log("SampleERC20 deployed to:", sampleERC20.address);
+
+  const MembershipNFT = await ethers.getContractFactory("MembershipERC721");
+  const membershipNFT = await MembershipNFT.deploy(
+    "BSMY",
+    "BSMY_",
+    sampleERC20.address,
+    "https"
+  );
+  await membershipNFT.deployed();
+  console.log("Membership NFTs deployed to:", membershipNFT.address);
+
+  await sampleERC20.transfer(signer2.address, 50000);
+  var balanceOfSigner1 = await sampleERC20.balanceOf(signer1.address);
+  var balanceOfSigner2 = await sampleERC20.balanceOf(signer2.address);
+  console.log("Balance of Signer 1:", balanceOfSigner1);
+  console.log("Balance of Signer 2:", balanceOfSigner2);
+
+  await sampleERC20.connect(signer2).approve(membershipNFT.address, 100000);
+
+  await membershipNFT.connect(signer2).redeem(2);
+  console.log("NFT reedemed");
+
+  balanceOfSigner1 = await sampleERC20.balanceOf(signer1.address);
+  balanceOfSigner2 = await sampleERC20.balanceOf(signer2.address);
+  console.log("Balance of Signer 1:", balanceOfSigner1);
+  console.log("Balance of Signer 2:", balanceOfSigner2);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
