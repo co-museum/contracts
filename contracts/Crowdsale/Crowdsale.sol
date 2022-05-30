cba9213d2d6722d9cf3b79989b3abbe195b4cf6e
Alaukik Pant <alaukikn.pant@gmail.com>
2022-05-30 16:47:09 +0800

    Use git submodules for ZpenZeppelin contracts

--- contracts/Crowdsale/Crowdsale.sol
+++ contracts/Crowdsale/Crowdsale.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+import "openzeppelin-contracts/contracts/utils/Context.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "hardhat/console.sol";
 
 /**
--- contracts/Crowdsale/TimedAllowanceCrowdsale.sol
+++ contracts/Crowdsale/TimedAllowanceCrowdsale.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "./Crowdsale.sol";
 import "./WhitelistContract.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/utils/math/Math.sol";
 
 /**
  * @title AllowanceCrowdsale
