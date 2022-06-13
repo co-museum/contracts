4f10561755a19231bf32693f59e2b08652f49840
Uros Perisic <uros.m.perisic@gmail.com>
2022-06-13 12:58:53 +0800

    Use OpenZeppelin from npm

--- contracts/Crowdsale/TimedAllowanceCrowdsale.sol
+++ contracts/Crowdsale/TimedAllowanceCrowdsale.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "./Crowdsale.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import "openzeppelin-contracts/contracts/utils/math/Math.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title AllowanceCrowdsale
