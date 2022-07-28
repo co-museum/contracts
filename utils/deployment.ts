5ea13a2c859e4b1ccff441921f21b6f36224be99
Uros Perisic <uros.m.perisic@gmail.com>
2022-07-28 18:03:29 +0800

    Fix deployment with NonceManager

--- utils/deployment.ts
+++ utils/deployment.ts
@@ -1,12 +1,12 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { BigNumberish } from 'ethers'
+import { BigNumberish, Signer } from 'ethers'
 import { ethers } from 'hardhat'
 import {
   AllowanceCrowdsale,
   ERC721,
   ERC721MembershipUpgradeable,
   ERC721Mock,
-  ERC721TitleDeed,
+  ERC721ArtNFT,
   ERC721VaultFactory,
   IERC20,
   IERC721,
@@ -20,6 +20,11 @@ const genesisTokenPrice = ethers.utils.parseUnits('40000', tokenVaultDecimals)
 const foundationTokenPrice = ethers.utils.parseUnits('4000', tokenVaultDecimals)
 const friendTokenPrice = ethers.utils.parseUnits('400', tokenVaultDecimals)
 
+async function getSignerOrDefault(signer?: Signer): Promise<Signer> {
+  const [defaultSigner] = await ethers.getSigners()
+  return signer ? Promise.resolve(signer) : Promise.resolve(defaultSigner)
+}
+
 export async function deployERC20Mock(
   holder: SignerWithAddress,
   name = 'ERC20',
@@ -32,29 +37,33 @@ export async function deployERC20Mock(
   return mockERC20.deployed()
 }
 
-export async function deploySettings(): Promise<Settings> {
+export async function deploySettings(signer?: Signer): Promise<Settings> {
   const Settings = await ethers.getContractFactory('Settings')
-  const settings = await Settings.deploy()
+  const sig = await getSignerOrDefault(signer)
+  const settings = await Settings.connect(sig).deploy()
   return settings.deployed()
 }
 
-export async function deployVaultFactory(settings?: Settings): Promise<ERC721VaultFactory> {
+export async function deployVaultFactory(settings?: Settings, signer?: Signer): Promise<ERC721VaultFactory> {
   settings = settings ? settings : await deploySettings()
   const VaultFactory = await ethers.getContractFactory('ERC721VaultFactory')
-  const vaultFactory = await VaultFactory.deploy(settings.address)
+  const sig = await getSignerOrDefault(signer)
+  const vaultFactory = await VaultFactory.connect(sig).deploy(settings.address)
   return vaultFactory.deployed()
 }
 
-export async function deployERC721Mock(name = 'Dummy NFT', symbol = 'DMY'): Promise<ERC721Mock> {
+export async function deployERC721Mock(name = 'Dummy NFT', symbol = 'DMY', signer?: Signer): Promise<ERC721Mock> {
   const DummyNFT = await ethers.getContractFactory('ERC721Mock')
-  const dummyNFT = await DummyNFT.deploy(name, symbol)
+  const sig = await getSignerOrDefault(signer)
+  const dummyNFT = await DummyNFT.connect(sig).deploy(name, symbol)
   return dummyNFT.deployed()
 }
 
-export async function deployERC721TitleDeed(recieverAddress: string): Promise<ERC721TitleDeed> {
-  const TitleDeedNFT = await ethers.getContractFactory('ERC721TitleDeed')
-  const titleDeedNFT = await TitleDeedNFT.deploy(recieverAddress)
-  return titleDeedNFT.deployed()
+export async function deployERC721ArtNFT(recieverAddress: string, signer?: Signer): Promise<ERC721ArtNFT> {
+  const ArtNFT = await ethers.getContractFactory('ERC721ArtNFT')
+  const sig = await getSignerOrDefault(signer)
+  const artNFT = await ArtNFT.connect(sig).deploy(recieverAddress)
+  return artNFT.deployed()
 }
 
 export async function deployTokenVault(
@@ -68,19 +77,24 @@ export async function deployTokenVault(
   tokenSupply = ethers.utils.parseUnits('4000000', tokenVaultDecimals),
   initialPrice = ethers.utils.parseUnits('4000000', tokenVaultDecimals),
   fee = 0,
+  signer?: Signer,
 ): Promise<TokenVault> {
-  const tx = await vaultFactory.mint(name, symbol, nft.address, usdc.address, nftId, tokenSupply, initialPrice, fee)
+  const sig = await getSignerOrDefault(signer)
+  const tx = await vaultFactory
+    .connect(sig)
+    .mint(name, symbol, nft.address, usdc.address, nftId, tokenSupply, initialPrice, fee)
   const receipt = await tx.wait()
   const [mintEvent] = receipt.events!.filter((event, i, arr) => event.event == 'Mint')
   const vaultAddress = mintEvent.args!['vault']
   return ethers.getContractAt('TokenVault', vaultAddress)
 }
 
-export async function deployVoteDelegator(tokenVault: TokenVault): Promise<VoteDelegator> {
+export async function deployVoteDelegator(tokenVault: TokenVault, signer?: Signer): Promise<VoteDelegator> {
+  const sig = await getSignerOrDefault(signer)
   const VoteDelegator = await ethers.getContractFactory('VoteDelegator')
-  const voteDelegator = await VoteDelegator.deploy()
+  const voteDelegator = await VoteDelegator.connect(sig).deploy()
   await voteDelegator.deployed()
-  await voteDelegator.initialize(tokenVault.address)
+  await voteDelegator.connect(sig).initialize(tokenVault.address)
   return Promise.resolve(voteDelegator)
 }
 
@@ -92,23 +106,27 @@ export async function deployMembership(
   genesisEnd = 2,
   foundationEnd = 4,
   friendEnd = 6,
+  signer?: Signer,
 ): Promise<ERC721MembershipUpgradeable> {
+  const sig = await getSignerOrDefault(signer)
   voteDelegator = voteDelegator ? voteDelegator : await deployVoteDelegator(tokenVault)
   const MembershipContract = await ethers.getContractFactory('ERC721MembershipUpgradeable')
-  const membershipContract = await MembershipContract.deploy()
+  const membershipContract = await MembershipContract.connect(sig).deploy()
   await membershipContract.deployed()
-  await membershipContract.initialize(
-    name,
-    symbol,
-    tokenVault.address,
-    voteDelegator.address,
-    genesisEnd,
-    foundationEnd,
-    friendEnd,
-    genesisTokenPrice,
-    foundationTokenPrice,
-    friendTokenPrice,
-  )
+  await membershipContract
+    .connect(sig)
+    .initialize(
+      name,
+      symbol,
+      tokenVault.address,
+      voteDelegator.address,
+      genesisEnd,
+      foundationEnd,
+      friendEnd,
+      genesisTokenPrice,
+      foundationTokenPrice,
+      friendTokenPrice,
+    )
   return Promise.resolve(membershipContract)
 }
 
@@ -118,9 +136,11 @@ export async function deployAllowanceCrowdsale(
   tokenHoldingWallet: SignerWithAddress,
   membershipContract: ERC721MembershipUpgradeable,
   acceptedStablecoins: IERC20[],
+  signer?: Signer,
 ): Promise<AllowanceCrowdsale> {
+  const sig = await getSignerOrDefault(signer)
   const AllowanceCrowdsale = await ethers.getContractFactory('AllowanceCrowdsale')
-  const allowanceCrowdsale = await AllowanceCrowdsale.deploy(
+  const allowanceCrowdsale = await AllowanceCrowdsale.connect(sig).deploy(
     tokenVault.address,
     treasuryWallet.address,
     tokenHoldingWallet.address,
