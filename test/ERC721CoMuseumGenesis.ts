a549e5fb9f57d89d52b1930b293092f1963f9c86
Alaukik Pant <alaukikn.pant@gmail.com>
2022-07-28 19:32:16 +0800

    fix broken tests

--- test/ERC721CoMuseumGenesis.ts
+++ test/ERC721CoMuseumGenesis.ts
@@ -1,31 +1,33 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { ERC721CoMuseumGenesis, ERC20Mock } from '../typechain'
+import { ERC721HonoraryMembership, ERC20Mock } from '../typechain'
 
 describe('ERC721CoMuseumGenesis', () => {
-  let erc721CoMuseumGenesis: ERC721CoMuseumGenesis
+  let erc721HonoraryMembership: ERC721HonoraryMembership
   let signer: SignerWithAddress
   let user1: SignerWithAddress
 
   beforeEach(async () => {
     ;[signer, user1] = await ethers.getSigners()
 
-    const ERC721CoMuseumGenesis = await ethers.getContractFactory('ERC721CoMuseumGenesis')
-    erc721CoMuseumGenesis = await ERC721CoMuseumGenesis.deploy(signer.address)
-    await erc721CoMuseumGenesis.deployed()
+    const ERC721HonoraryMembership = await ethers.getContractFactory('ERC721HonoraryMembership')
+    erc721HonoraryMembership = await ERC721HonoraryMembership.deploy(signer.address)
+    await erc721HonoraryMembership.deployed()
   })
 
-  describe('airdrop', () => {
-    it('can airdrop one NFT', async () => {
-      await erc721CoMuseumGenesis.airdrop(user1.address, 1)
-      expect(await erc721CoMuseumGenesis.ownerOf(0)).to.be.equal(user1.address)
+  describe('mint', () => {
+    it('can mint one NFT', async () => {
+      await erc721HonoraryMembership.mint(user1.address)
+      expect(await erc721HonoraryMembership.ownerOf(0)).to.be.equal(user1.address)
     })
 
-    it('can airdrop multiple NFTs', async () => {
-      await erc721CoMuseumGenesis.airdrop(user1.address, 5)
+    it('can mint multiple NFTs', async () => {
       for (let i = 0; i < 5; i++) {
-        expect(await erc721CoMuseumGenesis.ownerOf(i)).to.be.equal(user1.address)
+        await erc721HonoraryMembership.mint(user1.address)
+      }
+      for (let i = 0; i < 5; i++) {
+        expect(await erc721HonoraryMembership.ownerOf(i)).to.be.equal(user1.address)
       }
     })
   })
@@ -33,24 +35,26 @@ describe('ERC721CoMuseumGenesis', () => {
   describe('royalty', () => {
     it('can set default royalty', async () => {
       // 500 is 5 percent since _feeDenominator defaults to basis points
-      await expect(erc721CoMuseumGenesis.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await expect(erc721HonoraryMembership.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
     })
 
     it('can delete default royalty', async () => {
-      await expect(erc721CoMuseumGenesis.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await expect(erc721CoMuseumGenesis.deleteDefaultRoyalty()).to.not.be.reverted
+      await expect(erc721HonoraryMembership.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await expect(erc721HonoraryMembership.deleteDefaultRoyalty()).to.not.be.reverted
     })
 
     it('can set royalty for a specific token', async () => {
-      await expect(erc721CoMuseumGenesis.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await erc721CoMuseumGenesis.airdrop(user1.address, 5)
-      await expect(erc721CoMuseumGenesis.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
+      await expect(erc721HonoraryMembership.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await erc721HonoraryMembership.mint(user1.address)
+      await expect(erc721HonoraryMembership.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
     })
     it('can reset default royalty for a specific token to the global default', async () => {
-      await expect(erc721CoMuseumGenesis.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await erc721CoMuseumGenesis.airdrop(user1.address, 5)
-      await expect(erc721CoMuseumGenesis.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
-      await expect(erc721CoMuseumGenesis.resetTokenRoyalty(3)).to.not.be.reverted
+      await expect(erc721HonoraryMembership.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      for (let i = 0; i < 5; i++) {
+        await erc721HonoraryMembership.mint(user1.address)
+      }
+      await expect(erc721HonoraryMembership.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
+      await expect(erc721HonoraryMembership.resetTokenRoyalty(3)).to.not.be.reverted
     })
   })
 })
--- test/ERC721TitleDeed.ts
+++ test/ERC721TitleDeed.ts
@@ -1,31 +1,31 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { ERC721TitleDeed, ERC20Mock } from '../typechain'
+import { ERC721ArtNFT, ERC20Mock } from '../typechain'
 
 describe('ERC721TitleDeed', () => {
-  let erc721TitleDeed: ERC721TitleDeed
+  let erc721ArtNFT: ERC721ArtNFT
   let signer: SignerWithAddress
   let user1: SignerWithAddress
 
   beforeEach(async () => {
     ;[signer, user1] = await ethers.getSigners()
 
-    const ERC721TitleDeed = await ethers.getContractFactory('ERC721TitleDeed')
-    erc721TitleDeed = await ERC721TitleDeed.deploy(signer.address)
-    await erc721TitleDeed.deployed()
+    const ERC721ArtNFT = await ethers.getContractFactory('ERC721ArtNFT')
+    erc721ArtNFT = await ERC721ArtNFT.deploy(signer.address)
+    await erc721ArtNFT.deployed()
   })
 
   describe('airdrop', () => {
     it('can mint one NFT at a time', async () => {
-      await erc721TitleDeed.mint(signer.address)
-      expect(await erc721TitleDeed.ownerOf(0)).to.be.equal(signer.address)
+      await erc721ArtNFT.mint(signer.address)
+      expect(await erc721ArtNFT.ownerOf(0)).to.be.equal(signer.address)
     })
 
     it('can add multiple NFTs to the collection', async () => {
       for (let i = 0; i < 5; i++) {
-        await erc721TitleDeed.mint(signer.address)
-        expect(await erc721TitleDeed.ownerOf(i)).to.be.equal(signer.address)
+        await erc721ArtNFT.mint(signer.address)
+        expect(await erc721ArtNFT.ownerOf(i)).to.be.equal(signer.address)
       }
     })
   })
@@ -33,24 +33,24 @@ describe('ERC721TitleDeed', () => {
   describe('royalty', () => {
     it('can set default royalty', async () => {
       // 500 is 5 percent since _feeDenominator defaults to basis points
-      await expect(erc721TitleDeed.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await expect(erc721ArtNFT.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
     })
 
     it('can delete default royalty', async () => {
-      await expect(erc721TitleDeed.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await expect(erc721TitleDeed.deleteDefaultRoyalty()).to.not.be.reverted
+      await expect(erc721ArtNFT.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await expect(erc721ArtNFT.deleteDefaultRoyalty()).to.not.be.reverted
     })
 
     it('can set royalty for a specific token', async () => {
-      await expect(erc721TitleDeed.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await erc721TitleDeed.mint(signer.address)
-      await expect(erc721TitleDeed.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
+      await expect(erc721ArtNFT.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await erc721ArtNFT.mint(signer.address)
+      await expect(erc721ArtNFT.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
     })
     it('can reset default royalty for a specific token to the global default', async () => {
-      await expect(erc721TitleDeed.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
-      await erc721TitleDeed.mint(signer.address)
-      await expect(erc721TitleDeed.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
-      await expect(erc721TitleDeed.resetTokenRoyalty(3)).to.not.be.reverted
+      await expect(erc721ArtNFT.setDefaultRoyalty(signer.address, 500)).to.not.be.reverted
+      await erc721ArtNFT.mint(signer.address)
+      await expect(erc721ArtNFT.setTokenRoyalty(3, signer.address, 700)).to.not.be.reverted
+      await expect(erc721ArtNFT.resetTokenRoyalty(3)).to.not.be.reverted
     })
   })
 })
