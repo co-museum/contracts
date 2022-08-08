496b1caef6dc2c40a6ac6c2e2e24053df80ff2a0
Alaukik Pant <alaukikn.pant@gmail.com>
2022-08-08 15:50:53 +0800

    add escrow interactions

---
+++ scripts/interactions/AddEscrow.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'hardhat'
+import * as utils from '../../utils/deployment'
+import * as cfg from '../config'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+async function main() {
+  const [signer] = await ethers.getSigners()
+  const addressCfg: cfg.AddressConfig = cfg.loadConfig(cfg.ConfigEnv.address)
+  const escrowAddCfg: cfg.EscrowConfig = cfg.loadConfig(cfg.ConfigEnv.escrow)
+  const membership = await ethers.getContractAt('ERC721MembershipUpgradeable', addressCfg.ERC721MembershipUpgradeable!)
+  const tx = membership.connect(signer).addEscrowReleaseTime(escrowAddCfg.tokenIds, escrowAddCfg.timestamps!)
+  utils.printTx('Escrow add release time hash: ', (await tx).hash, utils.txType.tx)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
